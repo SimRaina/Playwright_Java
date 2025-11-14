81c46b97324179b2c6a3f401572f6ace14395445
Sim Raina QA <simran.pre90@gmail.com>
2025-11-14 19:17:59 +0100

    Added TestNG, upgraded playwright java version

--- src/test/java/com/test/pw/PlaywrightTests/FirstTest.java
+++ src/test/java/com/test/pw/PlaywrightTests/FirstTest.java
@@ -1,16 +1,41 @@
 package com.test.pw.PlaywrightTests;
 
 import com.microsoft.playwright.*;
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+import java.util.regex.Pattern;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class FirstTest {
-    public static void main(String[] args) {
-         	Playwright playwright = Playwright.create();
-         	Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions()
-            		.setHeadless(false).setSlowMo(50));
-            
-            Page page = browser.newPage();
+	
+	Playwright playwright;
+	Browser browser;
+	Page page;
+	
+	@BeforeTest
+	public void setup() {
+		playwright = Playwright.create();
+     	browser = playwright.chromium().launch(new BrowserType.LaunchOptions()
+        		.setHeadless(false));
+        
+        page = browser.newPage();	
+	}
+	
+	@AfterTest
+	public void tearDown() {
+		browser.close();
+		playwright.close();
+	}
+	
+	@Test
+    public void test1() {
             page.navigate("http://playwright.dev");
             System.out.println(page.title());
+            
+         // Expect a title "to contain" a substring.
+            assertThat(page).hasTitle(Pattern.compile("Playwright"));
           
         }
 }
